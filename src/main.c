f41dd68352fe6b1f9749597ccf734879e5fb0406
CyberSamurai0 <36649127+CyberSamurai0@users.noreply.github.com>
2024-10-23 11:02:55 -0400

    Add files

---
+++ src/main.c
@@ -0,0 +1,10 @@
+/*
+ * Point-to-Point Laser Diode Transmitter
+ * https://github.com/CyberSamurai0/P2P-Transmitter
+ */
+
+#include "networking.h"
+
+int main() {
+    sayHelloWorld();
+}
---
+++ src/networking.c
@@ -0,0 +1,8 @@
+// This file should be the entrypoint for physical layer encapsulation/decapsulation
+
+#include <stdio.h>
+#include "networking.h"
+
+void sayHelloWorld() {
+    printf("Hello World");
+}
