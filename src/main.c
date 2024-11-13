41a4e1dd696af65942ccce4a24879b6944894966
CyberSamurai0 <colinsaumure@gmail.com>
2024-11-13 01:47:46 -0500

    Testing stdio with Pi COM port

--- src/main.c
+++ src/main.c
@@ -10,28 +10,49 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#define TX_PIN 1
+#define RX_PIN 2
+
 int main() {
 	stdio_init_all();
 
-	sayHelloWorld();
+	// Initialise the Wi-Fi chip
+    // if (cyw43_arch_init()) {
+    //     printf("Wi-Fi init failed\n");
+    //     return -1;
+    // }
 
-//	int state = 0;
-//	while (state != -1) {
-//		scanf("%d", &state);
-//		if (state == 1) {
-//			pullUpDnControl(8, PUD_UP);
-//		}
-//	}
+	sayHelloWorld();
 
 //	char x[33];
 //	getMessage(x);
 //	Packet p = toPacket(x);
 //	printPacket(&p);
 
-	while (true) {
-        sayHelloWorld();
-        sleep_ms(5000);
-    }
+	// while (true) {
+    //     sayHelloWorld();
+    //     sleep_ms(5000);
+    // }
+
+	int state = 0;
+	while (state != 9) {
+		sleep_ms(1000);
+		printf("\n\n=====================[ CNIT 315 Point-to-Point Transmitter ]=====================\n\n");
+		printf("1. Turn Laser On\n");
+		printf("2. Turn Laser Off\n");
+		printf("\n9. Exit\n");
+		printf("\nSelection: ");
+		scanf("%d", &state);
+		switch (state) {
+			case 1:
+				printf("Console received ON");
+				break;
+			case 2:
+				printf("Console received OFF");
+				break;
+		}
+		sleep_ms(500);
+	}
 
 	return 0;
 }
