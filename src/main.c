711089931ea1580107dee3ab4d435d17d47ca2e7
CyberSamurai0 <36649127+CyberSamurai0@users.noreply.github.com>
2024-11-14 18:38:06 -0500

    Test controls

--- src/main.c
+++ src/main.c
@@ -9,13 +9,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
 
-#define TX_PIN 1
-#define RX_PIN 2
+#define TX_PIN 0
+#define RX_PIN 1
 
 int main() {
 	stdio_init_all();
 
+	gpio_init(TX_PIN);
+	gpio_set_dir(TX_PIN, GPIO_OUT);
+	gpio_put(TX_PIN, 1);
+	
+	// gpio_init(RX_PIN);
+	
 	// Initialise the Wi-Fi chip
     // if (cyw43_arch_init()) {
     //     printf("Wi-Fi init failed\n");
@@ -24,35 +31,38 @@ int main() {
 
 	sayHelloWorld();
 
-//	char x[33];
-//	getMessage(x);
-//	Packet p = toPacket(x);
-//	printPacket(&p);
+	// Packet* m = toPacket("The quick brown fox jumps over the lazy dog.");
+
+	while (true) {
+		sleep_ms(1000);
+		printf("Hello!");
+		// printPacket(m);
+	}
 
 	// while (true) {
-    //     sayHelloWorld();
-    //     sleep_ms(5000);
-    // }
+	// 	sleep_ms(1000);
+	// 	sendPacket(m);
+	// }
 
-	int state = 0;
-	while (state != 9) {
-		sleep_ms(1000);
-		printf("\n\n=====================[ CNIT 315 Point-to-Point Transmitter ]=====================\n\n");
-		printf("1. Turn Laser On\n");
-		printf("2. Turn Laser Off\n");
-		printf("\n9. Exit\n");
-		printf("\nSelection: ");
-		scanf("%d", &state);
-		switch (state) {
-			case 1:
-				printf("Console received ON");
-				break;
-			case 2:
-				printf("Console received OFF");
-				break;
-		}
-		sleep_ms(500);
-	}
+	// int state = 0;
+	// while (state != 9) {
+	// 	sleep_ms(1000);
+	// 	printf("\n\n=====================[ CNIT 315 Point-to-Point Transmitter ]=====================\n\n");
+	// 	printf("1. Turn Laser On\n");
+	// 	printf("2. Turn Laser Off\n");
+	// 	printf("\n9. Exit\n");
+	// 	printf("\nSelection: ");
+	// 	scanf("%d", &state);
+	// 	switch (state) {
+	// 		case 1:
+	// 			printf("Console received ON");
+	// 			break;
+	// 		case 2:
+	// 			printf("Console received OFF");
+	// 			break;
+	// 	}
+	// 	sleep_ms(500);
+	// }
 
 	return 0;
 }
