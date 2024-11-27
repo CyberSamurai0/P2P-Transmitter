539e55d605369477cba75bab7b80d358659f471b
CyberSamurai0 <CyberSamurai0@users.noreply.github.com>
2024-11-27 06:09:26 +0000

    deploy: 37d3aeef1538ddbb4421457e3536cd213260ee0d

--- search/all_0.js
+++ search/all_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['byte_0',['byte',['../structbyte.html',1,'']]],
-  ['bytes_2ec_1',['bytes.c',['../bytes_8c.html',1,'']]]
+  ['bytes_2ec_1',['bytes.c',['../bytes_8c.html',1,'']]],
+  ['bytes_2eh_2',['bytes.h',['../bytes_8h.html',1,'']]]
 ];
--- search/files_0.js
+++ search/files_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['bytes_2ec_0',['bytes.c',['../bytes_8c.html',1,'']]]
+  ['bytes_2ec_0',['bytes.c',['../bytes_8c.html',1,'']]],
+  ['bytes_2eh_1',['bytes.h',['../bytes_8h.html',1,'']]]
 ];
