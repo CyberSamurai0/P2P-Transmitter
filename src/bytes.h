4534d06dc80c729347812c1cc9941108feaac8a9
CyberSamurai0 <36649127+CyberSamurai0@users.noreply.github.com>
2024-11-26 19:24:27 -0500

    Add struct documentation

--- src/bytes.h
+++ src/bytes.h
@@ -3,15 +3,27 @@
 
 #include <stdint.h>
 
+/// @brief Double Linked List Node wrapper for an 8-bit integer
 typedef struct byte {
+	/// @brief 8-bit data portion of the Byte structure
 	uint8_t value;
+	
+	/// @brief The next Byte in a Packet sequence, or NULL if this is the last in the sequence
 	struct byte* next;
+
+	/// @brief The previous Byte in a Packet sequence, or NULL if this is the first in the sequence
 	struct byte* previous;
 } Byte;
 
+/// @brief Double Linked List structure with tracked length
 typedef struct packet {
+	/// @brief Number of contained Bytes within the Packet
 	int length;
+
+	/// @brief The first Byte contained within the Linked List, used for forward traversal
 	Byte* firstByte;
+
+	/// @brief The last Byte contained within the Linked List, used for reverse traversal
 	Byte* lastByte;
 } Packet;
 
