93c4e852270d34ef9bce60d0a03ae69f9defdd66
CyberSamurai0 <colinsaumure@gmail.com>
2024-12-11 01:36:25 -0500

    Remove unnecessary files

--- src/uart.h
+++
@@ -1,9 +0,0 @@
-#ifndef P2P_TRANSMITTER_UART_H
-#define P2P_TRANSMITTER_UART_H
-
-#include "packet.h"
-
-void sendMessage(char* m);
-void sendPacket(Packet* p, int pin);
-
-#endif //P2P_TRANSMITTER_UART_H
